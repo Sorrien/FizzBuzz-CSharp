b1a0599d737e12ca688b7bee99c3e88d7dcd3a34
csyye <csparks@yyesoftware.com>
2017-07-21 14:44:35 -0400

    Initial Commit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,9 +4,36 @@ namespace FizzBuzz
 {
     class Program
     {
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                string printString = "";
+                bool multipleOfFive = i % 5 == 0;
+                bool multipleOfThree = i % 3 == 0;
+
+                if (multipleOfFive || multipleOfThree)
+                {
+                    if (multipleOfThree)
+                    {
+                        printString += Fizz;
+                    }
+                    if (multipleOfFive)
+                    {
+                        printString += Buzz;
+                    }
+                }
+                else
+                {
+                    printString = i.ToString();
+                }
+
+
+                Console.Write(printString + " ");
+            }
+            Console.ReadLine();
         }
     }
 }
